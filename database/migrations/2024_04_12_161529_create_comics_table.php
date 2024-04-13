357f7c4638edd069277d6fd28c08bcc39d69c680
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-04-13 18:20:00 +0200

    seeding

--- database/migrations/2024_04_12_161529_create_comics_table.php
+++ database/migrations/2024_04_12_161529_create_comics_table.php
@@ -22,8 +22,8 @@ return new class extends Migration
             $table->string('series');
             $table->date('sale_date');
             $table->string('type');
-            $table->text('artists');
-            $table->text('writers');
+            $table->text('artists')->nullable();
+            $table->text('writers')->nullable();
         });
     }
 
